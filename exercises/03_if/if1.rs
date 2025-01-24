33cab70290fef74654cc500b47cd5a1cf050955d
k--chow <kchow423@gmail.com>
2025-01-23 23:22:36 -0500

    Init

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,10 +4,13 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if (a < b) { return b }
+    else { return a }
 }
 
 fn main() {
     // You can optionally experiment here.
+    
 }
 
 // Don't mind this for now :)
